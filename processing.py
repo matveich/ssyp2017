5537f2733f8864c81c48a00904beac96391e9df8
Konstantin <rybkin-k@mail.ru>
2017-07-14 23:40:35 +0700

    add wu-tsai alg

--- processing.py
+++ processing.py
@@ -94,7 +94,120 @@ class Process:
                 break
 
     def wu_tsai(self):
-        pass
+        # всего - 14 таблиц (a-n),
+        # 0 - белый пиксель (=0),
+        # 1 - чёрный пиксель (=1),
+        # 2 - текущий пиксель (=c),
+        # 3 - хотя бы один из них должен быть белым (=y),
+        # 4 - ничего не значащий пиксель (=x)
+
+        tables = [np.array([]) for _ in range(14)]
+
+        def init_tables():
+            tables[0] = np.array([
+                [1, 1, 3],
+                [1, 2, 0],
+                [1, 1, 3]]
+            )
+            tables[1] = np.array([
+                [1, 1, 1],
+                [1, 2, 1],
+                [3, 0, 3]]
+            )
+            tables[2] = np.array([
+                [3, 1, 1, 4],
+                [0, 2, 1, 1],
+                [3, 1, 1, 4]]
+            )
+            tables[3] = np.array([
+                [3, 0, 3],
+                [1, 2, 1],
+                [1, 1, 1],
+                [4, 1, 4]]
+            )
+            tables[4] = np.array([
+                [4, 0, 0],
+                [1, 2, 0],
+                [4, 1, 4]]
+            )
+            tables[5] = np.array([
+                [4, 1, 1],
+                [0, 2, 1],
+                [0, 0, 4]]
+            )
+            tables[6] = np.array([
+                [0, 1, 0],
+                [0, 2, 1],
+                [0, 0, 0]]
+            )
+            tables[7] = np.array([
+                [4, 1, 4],
+                [1, 2, 0],
+                [4, 0, 0]]
+            )
+            tables[8] = np.array([
+                [0, 0, 4],
+                [0, 2, 1],
+                [4, 1, 1]]
+            )
+            tables[9] = np.array([
+                [0, 0, 0],
+                [0, 2, 1],
+                [0, 1, 0]]
+            )
+            tables[10] = np.array([
+                [0, 0, 0],
+                [0, 2, 0],
+                [1, 1, 1]]
+            )
+            tables[11] = np.array([
+                [1, 0, 0],
+                [1, 2, 0],
+                [1, 0, 0]]
+            )
+            tables[12] = np.array([
+                [1, 1, 1],
+                [0, 2, 0],
+                [0, 0, 0]]
+            )
+            tables[13] = np.array([
+                [0, 0, 1],
+                [0, 2, 1],
+                [0, 0, 1]]
+            )
+
+        def comp_tables(arr, pattern):
+            white_px_flag = False
+            for _i in range(arr.shape[0]):
+                for _j in range(arr.shape[1]):
+                    if pattern[_i][_j] == 0 and arr[_i][_j] != 0:
+                        return False
+                    if pattern[_i][_j] == 1 and arr[_i][_j] != 1:
+                        return False
+                    if pattern[_i][_j] == 2 or pattern[_i][_j] == 4:
+                        continue
+                    if pattern[_i][_j] == 3 and arr[_i][_j] == 0:
+                        white_px_flag = True
+            return pattern[pattern == 3].size == 0 or white_px_flag
+        init_tables()
+        vec_func = np.vectorize(utils.bin_px)
+        flag = True
+        while flag:
+            flag = False
+            for i in range(1, self.h - 1):
+                for j in range(1, self.w - 1):
+                    if self.img[i][j] == 255:
+                        continue
+                    res = False
+                    for _table in tables[:2] + tables[4:]:
+                        res |= comp_tables(vec_func(self.img[i-1:i+2, j-1:j+2]), _table)
+                    if j < self.w - 2:
+                        res |= comp_tables(vec_func(self.img[i - 1:i + 2, j - 1:j + 3]), tables[2])
+                    if i < self.h - 2:
+                        res |= comp_tables(vec_func(self.img[i - 1:i + 3, j - 1:j + 2]), tables[3])
+                    if res:
+                        flag = True
+                        self.img[i][j] = 255
 
     def thinning(self):
         utils.save_debug_img(self.img, 'step1.png')
@@ -102,6 +215,8 @@ class Process:
         utils.save_debug_img(self.img, 'step2.png')
         self.zhang_suen()
         utils.save_debug_img(self.img, 'step3.png')
+        self.wu_tsai()
+        utils.save_debug_img(self.img, 'step4.png')
 
     # cc - connectivity components
 
---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+y = np.arange(35).reshape(5,7)
+print(y)
+print(y[1:, :4])
+print(y.shape[0])
